13b7f19383b67bc2fcc9947e99089ff958245fd9
AlSploit <131657888+AlSploit@users.noreply.github.com>
2025-03-24 07:53:58 -0400

    Delete AlSploit/Bedwars directory

--- AlSploit/Bedwars/MainScript.lua
+++
@@ -1,5 +0,0 @@
-local PlayersService = game:GetService("Players")
-
-local LocalPlayer = PlayersService.LocalPlayer
-
-LocalPlayer:Kick("This Version Of AlSploit Is No Longer Supported, Join The Disord To Get The New AlSploit | https://discord.gg/Msud9c5YRS")
